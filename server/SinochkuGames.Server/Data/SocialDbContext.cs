abdfc7b9d9967b1ddb089527514e0e29ff591b71
Nexar <96820799+Nexar69@users.noreply.github.com>
2026-09-19 11:05:20 +0200

    social server: add server/SinochkuGames.Server/Data/SocialDbContext.cs

---
+++ server/SinochkuGames.Server/Data/SocialDbContext.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SinochkuGames.Server.Data;
+
+public sealed class SocialDbContext(DbContextOptions<SocialDbContext> options)
+    : IdentityDbContext<AppUser>(options)
+{
+    public DbSet<Friendship> Friendships => Set<Friendship>();
+    public DbSet<FriendRequest> FriendRequests => Set<FriendRequest>();
+    public DbSet<DirectMessage> DirectMessages => Set<DirectMessage>();
+    public DbSet<NotificationEntity> Notifications => Set<NotificationEntity>();
+    public DbSet<GameSession> GameSessions => Set<GameSession>();
+    public DbSet<InviteCode> InviteCodes => Set<InviteCode>();
+    public DbSet<ProfileShowcase> ProfileShowcases => Set<ProfileShowcase>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Friendship>()
+            .HasIndex(x => new { x.UserAId, x.UserBId })
+            .IsUnique();
+
+        builder.Entity<FriendRequest>()
+            .HasIndex(x => new { x.SenderId, x.RecipientId, x.Status });
+
+        builder.Entity<DirectMessage>()
+            .HasIndex(x => new { x.SenderId, x.RecipientId, x.SentAtUtc });
+
+        builder.Entity<NotificationEntity>()
+            .HasIndex(x => new { x.UserId, x.CreatedAtUtc });
+
+        builder.Entity<GameSession>()
+            .HasIndex(x => new { x.UserId, x.StartedAtUtc });
+
+        builder.Entity<InviteCode>()
+            .HasKey(x => x.Code);
+
+        builder.Entity<ProfileShowcase>()
+            .HasIndex(x => new { x.UserId, x.SortOrder });
+    }
+}
